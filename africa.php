73414d14d8bc3438f7abd940bd6676c9c6245203
Timur Sharipov <timur.target@mail.ru>
2018-05-08 21:04:51 +0300

    The final stage

--- africa.php
+++ africa.php
@@ -1,20 +1,48 @@
 <?php
 
 $animalsContinent = [
-	'Asia' => ['Carcharocles megalodon', 'Elephas maximus', 'Bos javanicus'],
-	'Africa' => ['Syncerus caffer', 'Hippopotamus amphibius', 'Tragelaphus eurycerus'],
-	'North America' => ['Nasua narica', 'Antilocapra americana', 'Canis latrans'],
-	'South America' => ['Lama guanicoe', 'Hydrochoerus hydrochaeris', 'Panthera onca'],
+	'Asia' => ['Rhinocerotidae', 'Elephas maximus', 'Bos javanicus'],
+	'Africa' => ['Syncerus caffer', 'Hippopotamus amphibius', 'Hippotigris'],
+	'North America' => ['Bison', 'Antilocapra americana', 'Gulo'],
+	'South America' => ['Lutra', 'Hydrochoerus hydrochaeris', 'Panthera onca'],
 	'Antarctica' => ['Aptenodytes patagonicus', 'Leptonychotes weddellii', 'Physeter macrocephalus'],
-	'Europe' => ['Rangifer tarandus', 'Ursus arctos', 'Ovibos moschatus '],
-	'Australia' => ['Sarcophilus harrisii ', 'Phascolarctos cinereus ', 'Galeocerdo cuvier'],
+	'Europe' => ['Rangifer tarandus', 'Leporidae', 'Erinaceidae'],
+	'Australia' => ['Macropus', 'Phascolarctos cinereus', 'Galeocerdo cuvier'],
 ];
 
 foreach ($animalsContinent as $continent => $animals) {
-	echo '<h2>' $continent '</h2> <br>';
+	echo '<h2>', $continent, '</h2> <br>';
 	foreach ($animals as $animal) {
 		echo $animal, '<br>';
 	}
+}
+
+echo '<font color="red"> <h1> Новый массив: </h1> </font>';
+
+foreach ($animalsContinent as $continent => $animals) {
+	foreach ($animals as $animal) {
+		$result = str_word_count ($animal);
+		if ($result == 2) {
+		$new_array[]=$animal;
+		}
+	}
+}
+
+echo '<pre>';
+print_r($new_array);
+echo '<pre/>'; 
+
+echo '<font color="red"> <h1> Перемешиваем слова: </h1> </font>';
+
+foreach ($new_array as $animal) {
+	$newAnimal = explode (" ", $animal);
+	shuffle($newAnimal);
+	$newAnimal2 = implode (" ", $newAnimal);
+	
+	echo '<pre>';
+	print_r($newAnimal2);
+	echo '<pre/>'; 
+
 }
 
 ?>
